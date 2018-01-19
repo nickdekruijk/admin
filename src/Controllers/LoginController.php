84e0cc51aa13fe07094530c995b68beaeef375c0
Nick de Kruijk <git@nickdekruijk.nl>
2018-01-19 15:12:29 +0100

    PSR-2 requires 'public static' and change default redirect path

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -6,11 +6,26 @@ use Route;
 
 class LoginController extends \App\Http\Controllers\Auth\LoginController
 {
-    static public function routes()
+    /**
+     * Set the LaraPages authentication routes.
+     *
+     * This is called by the LaraPages::loginroutes() Facade function
+     */
+    public static function routes()
     {
         Route::get(config('larapages.adminpath').'/login', '\LaraPages\Admin\Controllers\LoginController@showLoginForm')->name('login');
         Route::post(config('larapages.adminpath').'/login', '\LaraPages\Admin\Controllers\LoginController@login');
         Route::post(config('larapages.adminpath').'/logout', '\LaraPages\Admin\Controllers\LoginController@logout')->name('logout');
+    }
+
+    /**
+     * Where to redirect users after login.
+     *
+     * Change the value from \App\Http\Controllers\Auth\LoginController to the larapages.adminpath config
+     */
+    public function __construct()
+    {
+        $this->redirectTo = '/'.config('larapages.adminpath');
     }
 
     /**
