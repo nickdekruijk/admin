c2cb2f5cfba9632536bd6e99fa7f281e9a7371ae
Nick de Kruijk <git@nickdekruijk.nl>
2018-01-25 17:18:13 +0100

    Added main navigation buttons to Dashboard

--- src/views/dashboard.blade.php
+++ src/views/dashboard.blade.php
@@ -1,7 +1,10 @@
 @extends('larapages::base')
-    
+
 @section('view')
         <section class="fullpage">
             <h2>{{ trans('larapages::base.welcome_back') }} {{ Auth::user()->name }}</h2>
+            <div class="dashboard">
+                {!! $lp->navigation() !!}
+            </div>
         </section>
 @endsection
