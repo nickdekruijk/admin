c7d149acf1429d0b4fa04956931ced6d042b466e
Nick de Kruijk <git@nickdekruijk.nl>
2018-07-16 14:44:21 +0200

    Rename $n to $index

--- src/Images.php
+++ src/Images.php
@@ -4,23 +4,23 @@ namespace NickDeKruijk\Admin;
 
 trait Images {
 
-    public function images($column = 'images', $n = null)
+    public function images($column = 'images', $index = null)
     {
         if (empty($this->$column) || !trim($this->$column)) {
             return null;
         }
         $images = explode(chr(10), trim($this->$column));
-        return $n !== null ? explode('|', trim($images[$n]), 2) : $images;
+        return $index !== null ? explode('|', trim($images[$index]), 2) : $images;
     }
 
-    public function image($column = 'images', $n = 0)
+    public function image($column = 'images', $index = 0)
     {
-        return trim($this->images($column, $n)[0]) ?: null;
+        return trim($this->images($column, $index)[0]) ?: null;
     }
 
-    public function imageCaption($column = 'images', $n = 0)
+    public function imageCaption($column = 'images', $index = 0)
     {
-        return trim($this->images($column, $n)[1] ?? null) ?: null;
+        return trim($this->images($column, $index)[1] ?? null) ?: null;
     }
 
     public function imageCount($column = 'images')
