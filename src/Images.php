d2d1d1e8acf9b2658519023de878ac189f7dc154
Nick de Kruijk <git@nickdekruijk.nl>
2018-07-16 14:38:32 +0200

    Better null/error handling

--- src/Images.php
+++ src/Images.php
@@ -6,21 +6,21 @@ trait Images {
 
     public function images($column = 'images', $n = null)
     {
-        if (empty(trim($this->$column))) {
+        if (empty($this->$column) || !trim($this->$column)) {
             return null;
         }
-        $images = array_map('trim', explode(chr(10), trim($this->$column)));
-        return $n !== null ? explode('|', $images[$n], 2) : $images;
+        $images = explode(chr(10), trim($this->$column));
+        return $n !== null ? explode('|', trim($images[$n]), 2) : $images;
     }
 
     public function image($column = 'images', $n = 0)
     {
-        return trim($this->images($column, $n)[0]);
+        return trim($this->images($column, $n)[0]) ?: null;
     }
 
     public function imageCaption($column = 'images', $n = 0)
     {
-        return trim($this->images($column, $n)[1]);
+        return trim($this->images($column, $n)[1] ?? null) ?: null;
     }
 
 }
