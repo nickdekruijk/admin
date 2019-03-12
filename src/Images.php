65ded0950ff3865fe32cd262cf1bed01c08ea367
Nick de Kruijk <git@nickdekruijk.nl>
2019-03-12 17:00:00 +0100

    Updated Images trait (might need code update)

--- src/Images.php
+++ src/Images.php
@@ -6,6 +6,11 @@ trait Images {
 
     public function images($column = null, $index = null)
     {
+        return $this->imagesParse($column, $index, true);
+    }
+
+    public function imagesParse($column = null, $index = null, $array = false)
+    {
         if (is_numeric($column)) {
             $index = $column;
             $column = null;
@@ -17,22 +22,33 @@ trait Images {
             return null;
         }
         $images = explode(chr(10), trim($this->$column));
+        if ($array) {
+            $array = [];
+            foreach($images as $image) {
+                $image = explode('|', $image);
+                $array[] = [
+                    'file' => $image[0],
+                    'caption' => $image[1] ?? null,
+                ];
+            }
+            return $array;
+        }
         return $index !== null ? explode('|', trim($images[$index]), 2) : $images;
     }
 
     public function image($column = null, $index = 0)
     {
-        return trim($this->images($column, $index)[0]) ?: null;
+        return trim($this->imagesParse($column, $index)[0]) ?: null;
     }
 
     public function imageCaption($column = null, $index = 0)
     {
-        return trim($this->images($column, $index)[1] ?? null) ?: null;
+        return trim($this->imagesParse($column, $index)[1] ?? null) ?: null;
     }
 
     public function imageCount($column = null)
     {
-        return count($this->images($column));
+        return count($this->imagesParse($column));
     }
 
 }
