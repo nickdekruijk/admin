c45581fc47ec0f5ddb645f416e467b38f47ede2c
Nick de Kruijk <git@nickdekruijk.nl>
2020-03-03 17:30:10 +0100

    Formatting

--- src/Controllers/ReportController.php
+++ src/Controllers/ReportController.php
@@ -11,8 +11,8 @@ class ReportController extends BaseController
         $lp->checkSlug($lp->slug, 'read');
         $response = '<ul>';
         foreach ($lp->module('queries') as $queryId => $query) {
-            $response .= '<li data-url="'.route('report', ['slug' => $lp->slug, 'id' => str_slug($queryId)]).'">';
-            $response .= '<div><i></i><span>'.$queryId.'</span></div>';
+            $response .= '<li data-url="' . route('report', ['slug' => $lp->slug, 'id' => str_slug($queryId)]) . '">';
+            $response .= '<div><i></i><span>' . $queryId . '</span></div>';
             $response .= '</li>';
         }
         $response .= '</ul>';
@@ -23,22 +23,22 @@ class ReportController extends BaseController
     {
         foreach ($this->module('queries') as $queryId => $query) {
             if (str_slug($queryId) == $id) {
-                if (env('DB_CONNECTION')=='mysql') $set = DB::select('SET SESSION group_concat_max_len = 1024000');
+                if (env('DB_CONNECTION') == 'mysql') $set = DB::select('SET SESSION group_concat_max_len = 1024000');
                 if (is_array($query)) {
                     $data =  DB::select($query['query']);
                     // Apply json_decode() if needed
                     if (isset($query['json'])) {
                         foreach (explode(',', $query['json']) as $json) {
-                            foreach($data as $id => $row) {
-                                $data[$id]->$json = (array)json_decode($row->$json);
+                            foreach ($data as $id => $row) {
+                                $data[$id]->$json = (array) json_decode($row->$json);
                             }
                         }
                         if (!isset($query['index'])) {
                             $index = [];
-                            foreach($data as $id => $row) {
-                                foreach($row as $key => $value) {
+                            foreach ($data as $id => $row) {
+                                foreach ($row as $key => $value) {
                                     if (is_array($value)) {
-                                        foreach($value as $key2 => $value2) {
+                                        foreach ($value as $key2 => $value2) {
                                             $index[$key . '.' . $key2] = $key . '.' . $key2;
                                         }
                                     } else {
@@ -57,7 +57,7 @@ class ReportController extends BaseController
                             $as = $index[1] ?? null;
                             $index = explode('.', $index[0]);
                             foreach ($data as $id => $row) {
-                                $row = (array)$row;
+                                $row = (array) $row;
                                 if (!isset($indexed[$id])) {
                                     $indexed[$id] = [];
                                 }
@@ -99,11 +99,11 @@ class ReportController extends BaseController
             }
             $response .= '<tr>';
             foreach ($row as $column) {
-                $response .= '<td><div>'.htmlspecialchars($column).'</div></td>';
+                $response .= '<td><div>' . htmlspecialchars($column) . '</div></td>';
             }
             $response .= '</tr>';
         }
-        return '<table class="report">'.$response.'</table>';
+        return '<table class="report">' . $response . '</table>';
     }
 
     private function safe_get_object_vars($data)
@@ -121,12 +121,12 @@ class ReportController extends BaseController
         $data = $this->getQueryData($slug, $id);
         $out = fopen('php://memory', 'w');
         fputcsv($out, array_keys($this->safe_get_object_vars($data[0])));
-        foreach($data as $line) {
+        foreach ($data as $line) {
             fputcsv($out, $this->safe_get_object_vars($line), $this->module('download_csv_delimiter') ?: ',');
         }
         rewind($out);
         $csv = stream_get_contents($out);
         fclose($out);
-        return response($csv)->header('Content-type', 'text/csv')->header('Content-disposition', 'attachment;filename='. $id . '.csv');
+        return response($csv)->header('Content-type', 'text/csv')->header('Content-disposition', 'attachment;filename=' . $id . '.csv');
     }
 }
