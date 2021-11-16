835c4640ae35c04f17be62694006ddab0062af6d
Nick de Kruijk <git@nickdekruijk.nl>
2021-11-16 11:06:35 +0100

    AdminServiceProvider

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace NickDeKruijk\Admin;
+
+use \Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadViewsFrom(__DIR__ . '/views', 'admin');
+        // $this->publishes([
+        //     __DIR__ . '/config.php' => config_path('admin.php'),
+        // ], 'config');
+        // $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        // if (config('admin.role_column_migration')) {
+        //     $this->loadMigrationsFrom(__DIR__ . '/migrations/role_column');
+        // }
+        // $this->loadTranslationsFrom(__DIR__ . '/lang', 'admin');
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         UserCommand::class,
+        //     ]);
+        // }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__ . '/config.php', 'admin');
+    }
+}
