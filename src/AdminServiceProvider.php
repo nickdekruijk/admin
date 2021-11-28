cf54a15df6d82a6136a6ee9cfd8f121cbf40aa7d
Nick de Kruijk <git@nickdekruijk.nl>
2021-11-28 23:26:14 +0100

    Routes

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -14,11 +14,13 @@ class AdminServiceProvider extends ServiceProvider
     public function boot()
     {
         // $this->loadViewsFrom(__DIR__ . '/views', 'admin');
-        // $this->loadRoutesFrom(__DIR__ . '/routes.php');
         // Enable publishing of config file.
         $this->publishes([
             __DIR__ . '/config.php' => config_path('admin.php'),
         ], 'config');
+
+        // Load the routes needed for admin.
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
         // if (config('admin.role_column_migration')) {
         //     $this->loadMigrationsFrom(__DIR__ . '/migrations/role_column');
         // }
---
+++ src/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use NickDeKruijk\Admin\Controllers\AdminController;
+use NickDeKruijk\Admin\Controllers\AssetController;
+use NickDeKruijk\Admin\Livewire\Login;
+
+Route::group(['middleware' => 'web'], function () {
+    Route::get(config('admin.route_prefix') . '/admin.css', [AssetController::class, 'stylesheets'])->name('admin.css');
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get(config('admin.route_prefix') . '/login', Login::class)->name('admin.login');
+    });
+    Route::post(config('admin.route_prefix') . '/logout', [AdminController::class, 'logout'])->name('admin.logout');
+    Route::get(config('admin.route_prefix') . '/{module?}', [AdminController::class, 'index'])->name('admin.index');
+});
