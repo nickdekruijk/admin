1e490f5109d8600154a378182762cfbe3a92dfe9
Nick de Kruijk <git@nickdekruijk.nl>
2021-12-05 14:39:57 +0100

    change route parameter name

--- src/routes.php
+++ src/routes.php
@@ -8,5 +8,5 @@ Route::group(['middleware' => 'web'], function () {
     Route::get(config('admin.route_prefix') . '/admin.css', [AssetController::class, 'stylesheets'])->name('admin.css');
     Route::get(config('admin.route_prefix') . '/login', Login::class)->name('admin.login');
     Route::post(config('admin.route_prefix') . '/logout', [AdminController::class, 'logout'])->name('admin.logout');
-    Route::get(config('admin.route_prefix') . '/{module?}', [AdminController::class, 'index'])->name('admin.index');
+    Route::get(config('admin.route_prefix') . '/{component?}', [AdminController::class, 'index'])->name('admin.index');
 });
