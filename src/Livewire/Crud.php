2dc858919d0b9a33af75a3aa89dbcdbf9429165f
Nick de Kruijk <git@nickdekruijk.nl>
2021-12-10 22:43:10 +0100

    Enable edit/create mode

--- src/Livewire/Crud.php
+++ src/Livewire/Crud.php
@@ -9,6 +9,8 @@ class Crud extends Component
 {
     public $module;
     public $listview;
+    public $editing = false;
+    public $data;
 
     public function mount($admin)
     {
@@ -24,5 +26,21 @@ class Crud extends Component
     public static function render()
     {
         return view('admin::livewire.crud');
+    }
+
+    public function create()
+    {
+        $this->editing = 0;
+    }
+
+    public function edit($id)
+    {
+        $this->data = $this->module->findOrFail($id);
+        $this->editing = $id;
+    }
+
+    public function close()
+    {
+        $this->editing = false;
     }
 }
--- src/views/livewire/crud.blade.php
+++ src/views/livewire/crud.blade.php
@@ -1,6 +1,7 @@
-<div>
+<div class="{{ $editing !== false ? 'editing' : '' }}">
     <header>
         <h2>@lang($module->getAdminConfig()->title)</h2>
+        <button wire:click="create"><i class="fa-solid fa-plus"></i>@lang('New')</button>
     </header>
     <section class="listview">
         <table>
@@ -9,13 +10,21 @@
                     <th>@lang($column)</th>
                 @endforeach
             </tr>
-            @foreach($module->all($listview) as $row)
-                <tr>
+            @foreach($module->all(array_merge(['id'], $listview)) as $row)
+                <tr class="{{ $row->id == $editing ? 'active' : '' }}" wire:click="edit({{ $row->id }})">
                     @foreach($listview as $column) 
                         <td>{{ $row->$column }}</td>
                     @endforeach
                 </tr>
             @endforeach
         </table>
+    </section>
+    <section class="editor">
+        <div class="buttons">
+            <button wire:click="close"><i class="fa-solid fa-ban"></i>@lang('Close')</button>
+            @if ($editing)
+                <span class="id">{{ $editing }}</id>
+            @endif
+        </div>
     </section>
 </div>
