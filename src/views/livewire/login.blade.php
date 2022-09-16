161c7fc3d5f7678414edba36d8a4713d8bbc0535
Nick de Kruijk <git@nickdekruijk.nl>
2022-09-16 22:04:04 +0200

    Login style tweaks

--- src/views/livewire/login.blade.php
+++ src/views/livewire/login.blade.php
@@ -1,11 +1,11 @@
 <div class="flex flex-center t-white relative mw-full p-2">
     <div class="bg-normal rounded mw-full">
-        <header class="bg-light fs-header px-3 py-1 logo fw-300">
+        <header class="bg-light fs-header py-1 px-3 logo fw-300">
             {!! config('admin.logo') !!}
         </header>
-        <form class="form px-3 py-2" wire:submit.prevent="submit">
+        <form class="form p-3" wire:submit.prevent="submit">
             @foreach(config('admin.credentials') as $column)
-                <label class="block py-1">
+                <label class="block pb-2">
                     @lang(ucfirst($column == 'email' ? 'e-mail address' : $column))
                     <input class="input" size="30" type="{{ $column=='password' ? 'password' : 'text' }}" wire:model.defer="{{ $column }}" {{ $loop->iteration==1 ? 'autofocus' : '' }}>
                 </label>
@@ -19,7 +19,7 @@
                     </ul>
                 </div>
             @endif
-            <button class="input button" type="submit" wire:click="submit"><i class="fa-solid fa-right-to-bracket"></i>@lang('Login')</button>
+            <button class="input button" type="submit" wire:click="submit"><i class="icon fa-solid fa-right-to-bracket"></i>@lang('Login')</button>
         </form>
     </div>
 </div>
