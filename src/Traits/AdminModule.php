e620c4f257e99ab201e48b6a9898475e31a8b579
Nick de Kruijk <git@nickdekruijk.nl>
2022-09-18 11:42:59 +0200

    wip

--- src/Traits/AdminModule.php
+++ src/Traits/AdminModule.php
@@ -8,29 +8,53 @@ use NickDeKruijk\Admin\Classes\AdminConfig;
 
 trait AdminModule
 {
-    private AdminConfig $admin_config;
+    private $admin_config;
 
-    public function __construct()
+    // public function __construct()
+    // {
+    //     parent::__construct();
+
+    //     // Create new AdminConfig instance.
+    //     $this->admin_config = new AdminConfig($this);
+
+    //     // If the Class that uses the AdminModule trait has a $adminConfig property merge them
+    //     $this->setAdminConfig();
+
+    //     // If the Class that uses the AdminModule trait has a $adminConfig property merge them
+    //     if (isset($this->adminConfig)) {
+    //         foreach ($this->adminConfig as $key => $value) {
+    //             $this->admin_config->$key = $value;
+    //         }
+    //     }
+    // }
+
+    /**
+     * Returns the complete AdminConfig object or one of its keys
+     *
+     * @param string|null $key  The AdminConfig key to return, use null to return everything.
+     * @return AdminConfig|string|array
+     */
+    public function getAdminConfig(string $key = null): AdminConfig|string|array
     {
-        parent::__construct();
-
-        // Create new AdminConfig instance.
-        $this->admin_config = new AdminConfig($this);
+        if (!$this->admin_config) {
+            $this->admin_config = new AdminConfig($this);
+        }
+        return $key ? $this->admin_config->$key : $this->admin_config;
     }
 
     /**
-     * Returns the AdminConfig object, modules can overwrite this function.
+     * Classes that use the AdminModule trait can overwrite this function to update config
      *
-     * @return AdminConfig
+     * @return void
      */
-    public function getAdminConfig(): AdminConfig
+    public function setAdminConfig(): void
     {
-        return $this->admin_config;
+        // $this->admin_config->title = 'New title';
     }
 
     /**
      * Merge the current AdminConfig object with new and/or updated attributes.
-     * Useful for usage in overwriting the getAdminConfig function.
+     * Useful for usage in overwriting the setAdminConfig function.
      *
      * @param array $attributes
      * @return AdminConfig
